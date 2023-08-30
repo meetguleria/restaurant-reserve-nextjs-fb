8d6f18aaaf1c2f6a3b05587cdfbe58fb01260ca4
Meet Guleria <meetguleria94@gmail.com>
2023-08-29 20:46:06 -0700

    modal-fixes

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { handleSignUp}
+import { handleSignUp, handleLogin, handleGoogleSignIn  } from '../src/components/authFunctions';
 
 const Auth: React.FC = () => {
     const [email, setEmail] = useState<string>('');
@@ -9,8 +9,8 @@ const Auth: React.FC = () => {
         <div>
             <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
             <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleSignUp}>Sign Up</button>
-            <button onClick={handleLogin}>Login</button>
+            <button onClick={() => handleSignUp(email, password)}>Sign Up</button>
+            <button onClick={() => handleLogin(email, password)}>Login</button>
             <button onClick={handleGoogleSignIn}>Sign in with Google</button>
         </div>
     );
--- src/components/authModal.tsx
+++ src/components/authModal.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
+import { handleSignUp, handleLogin, handleGoogleSignIn } from './authFunctions';
 
-type AuthModalProps = {
-    toggleModal: () => void;
-};
+interface AuthModalProps {
+    closeModal: () => void;
+}
 
 const AuthModal: React.FC<AuthModalProps> = ({ closeModal }) => {
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg">
-        <button onClick={closeModal} className="float-right">
-          X
-        </button>
-        <h2 className="text-xl mb-4">Sign In or Sign Up</h2>
-        {/* Add your sign-in and sign-up form or buttons here */}
-      </div>
-    </div>
-  );
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg">
+                <button onClick={closeModal} className="float-right"> {/* Use closeModal */}
+                    X
+                </button>
+                <h2 className="text-xl mb-4">Sign In or Sign Up</h2>
+
+                {/* Sign-in and sign-up form */}
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    className="block w-full mb-2"
+                />
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    className="block w-full mb-4"
+                />
+                <button onClick={() => handleSignUp(email, password)} className="block w-full mb-2">
+                    Sign Up
+                </button>
+                <button onClick={() => handleLogin(email, password)} className="block w-full mb-2">
+                    Login
+                </button>
+                <button onClick={handleGoogleSignIn} className="block w-full">
+                    Sign in with Google
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default AuthModal;
